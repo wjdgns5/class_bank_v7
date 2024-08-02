214ec6192e979485bbb74eac6301f0c79ecd52c3
wjdgns5 <rhfovkeh5@naver.com>
2024-08-02 15:43:31 +0900

    first commit

---
+++ src/main/java/com/tenco/bank/ClassBankApplication.java
@@ -0,0 +1,13 @@
+package com.tenco.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tenco/bank/ClassBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenco.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
